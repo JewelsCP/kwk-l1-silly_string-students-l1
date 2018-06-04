2af83a18eb4f8d62bd80f4be703692633b7ec773
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-04 20:01:30 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -43,6 +43,7 @@ puts proper_sentence
 
 # Challenge 7: String Switcheroo
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
+puts.supercalifragilisticexpialidocious
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
